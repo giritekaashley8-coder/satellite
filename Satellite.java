6840269a4f618f546f89f615366b125a3b6b3684
giritekaashley8-coder <giritekaashley8@gmail.com>
2026-09-25 17:19:49 +0200

    Update Satellite.java

--- Satellite.java
+++ Satellite.java
@@ -1,5 +1,5 @@
 public class Satellite {
-    public static int[][] readImage(java.util.Scanner s, int n, int m) {
+    public static int[][] read(java.util.Scanner s, int n, int m) {
         int[][] img = new int[n][m];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
@@ -7,27 +7,22 @@ public class Satellite {
         return img;
     }
 
-    public static int[] check(int i, int j, int[] r) {
-        int x1 = Math.min(r[0], i + 1);
-        int y1 = Math.min(r[1], j + 1);
-        int x2 = Math.max(r[2], i + 1);
-        int y2 = Math.max(r[3], j + 1);
-        return new int[]{x1, y1, x2, y2};
-    }
-
-    public static int[] findRect(int[][] a, int[][] b, int n, int m) {
-        int[] r = {n + 1, m + 1, 0, 0};
+    public static int[] find(int[][] a, int[][] b, int n, int m) {
+        int x1 = n, y1 = m, x2 = 0, y2 = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (a[i][j] != b[i][j]) r = check(i, j, r);
-        return r;
+                if (a[i][j] != b[i][j]) {
+                    if (i < x1) x1 = i; if (j < y1) y1 = j;
+                    if (i > x2) x2 = i; if (j > y2) y2 = j;
+                }
+        return new int[]{x1 + 1, y1 + 1, x2 + 1, y2 + 1};
     }
 
     public static void main(String[] args) {
         java.util.Scanner s = new java.util.Scanner(System.in);
         int n = s.nextInt(), m = s.nextInt();
-        int[][] a = readImage(s, n, m), b = readImage(s, n, m);
-        int[] r = findRect(a, b, n, m);
+        int[][] a = read(s, n, m), b = read(s, n, m);
+        int[] r = find(a, b, n, m);
         System.out.println(r[0] + " " + r[1] + " " + r[2] + " " + r[3]);
     }
 }
